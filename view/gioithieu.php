d941414cbe1e11c4fd22e3d5b77f90ddf20a6188
quangdat83 <163096717+quangdat83@users.noreply.github.com>
2024-03-29 15:10:16 +0700

    Delete view directory

--- view/gioithieu.php
+++
@@ -1,6 +0,0 @@
-<div class="mb">
-    <div class="box_title">Gioi thieu</div>
-    <div class="box_content">
-
-    </div>
-</div>
--- view/lienhe.php
+++
@@ -1,6 +0,0 @@
-<div class="mb">
-    <div class="box_title">Lien he</div>
-    <div class="box_content">
-
-    </div>
-</div>
